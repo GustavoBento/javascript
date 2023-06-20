224bac80daeb7776044407f584aa8fb01b37f458
Gustavo Bento <133028031+GustavoBento@users.noreply.github.com>
2023-06-20 14:34:53 -0300

    aula 11

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 50.5
+console.log(`Sua velocidade é ${vel}KM/h`)
+if (vel > 60) {
+    console.log('Você está acima da velocidade permitida. MULTADO!')
+}
+console.log('Dirija com segurança, use sempre o cinto de segurança!')
