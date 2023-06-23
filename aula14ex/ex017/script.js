a0c69812404777b78f4efd680078864b03c95fea
Gustavo Bento <133028031+GustavoBento@users.noreply.github.com>
2023-06-23 16:42:08 -0300

    aula14 ex02

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,16 @@
-alert('ola')
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+        tab.innerHTML = ''
+        let n = Number(num.value)
+        for(let c = 1; c <= 10; c++) {
+            let item = document.createElement('option')
+            item.text = `${n} X ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+        }
+    }
+}
